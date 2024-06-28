a315f953dbe410494db393d2c0a669767f8d0fdf
Art Vardanyan <akvardanyan99@gmail.com>
2024-06-27 22:38:01 -0400

    hide bird until play

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-null
+const score = document.querySelector('.score-value');
+const bird = document.querySelector('.bird');
+const startMessage = document.querySelector('.start-message');
+
+const gameState = 'start';
+
+gameState === 'start' 
+    ? bird.style.display = 'none'
+    : startMessage.style.display = 'block'
+
+
