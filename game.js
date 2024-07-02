9bddac265c005cd6f2dc8584e79f4b4022f42723
Art Vardanyan <akvardanyan99@gmail.com>
2024-07-02 19:57:30 -0400

    gravity working

--- game.js
+++ game.js
@@ -1,39 +1,64 @@
 window.onload = () => {
     document.body.style.visibility = 'visible';
 };
-
 const scoreBox = document.querySelector('.score');
 const score = document.querySelector('.score-value');
 const bird = document.querySelector('.bird');
 const startMessage = document.querySelector('.start-message');
 const gameOverMessage = document.querySelector('.game-over');
-
 const ship = document.querySelector('#ships1');
+const gameStates = {
+    start: 'start',
+    play: 'play',
+    crash: 'crash'
+}
 
-let gameState = 'start';
-
-//hide the bird
-bird.style.display = 'none';
-
-//hide the score box
-scoreBox.style.display = 'none';
-
-//hide the game over message
-gameOverMessage.style.display = 'none';
-
-//hide the ship
-// ship.style.display = 'none';
-
-// ship.style.left = '100%';
+let mode = gameStates.start;
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        gameState = 'play';
-        startMessage.style.display = 'none';
-        bird.style.display = 'block';
-        scoreBox.style.display = 'block';
+        startGame();
+
+        
     }
 });
+
+function startGame () {
+    gameState = 'play';
+    //hide start message
+    startMessage.style.display = 'none';
+    //show the score box
+    scoreBox.style.display = 'block';
+    //show the bird and allow it to fly
+    bird.style.display = 'block';
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === ' ') {
+
+        }
+    })
+
+    runShips();
+    startGravity()
+}
+
+
+
+
+function startGravity () {
+    let velocity = 0;
+    const gravityConstant = 0.25;
+    
+    function gravity () {
+        velocity += gravityConstant;
+        let position = parseInt(getComputedStyle(bird).top) + velocity;
+        bird.style.top = `${position}px`;
+    
+        requestAnimationFrame(gravity);
+    }
+
+    requestAnimationFrame(gravity);
+}
 
 function runShips () {
 
@@ -83,4 +108,4 @@ function clearShips () {
     }, 10000)
 }
 
-// runShips()
+// startGame();
